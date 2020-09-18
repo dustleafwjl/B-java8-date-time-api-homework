4302b20f99337eb243953e31daf07f48088cb5c4
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-18 15:38:13 +0800

    [jianglin] fix: zoneId asia/shanghai

--- src/main/java/com/thoughtworks/capability/gtb/MeetingSystemV3.java
+++ src/main/java/com/thoughtworks/capability/gtb/MeetingSystemV3.java
@@ -3,21 +3,10 @@ package com.thoughtworks.capability.gtb;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
-/**
- * 脑洞会议系统v3.0
- * 1.当前会议时间"2020-04-01 14:30:00"表示伦敦的本地时间，而输出的新会议时间是芝加哥的本地时间
- *   场景：
- *   a:上个会议是伦敦的同事定的，他在界面上输入的时间是"2020-04-01 14:30:00"，所以我们要解析的字符串是伦敦的本地时间
- *   b:而我们在当前时区(北京时区)使用系统
- *   c:我们设置好新会议时间后，要发给芝加哥的同事查看，所以格式化后的新会议时间要求是芝加哥的本地时间
- * 2.用Period来实现下个会议时间的计算
- *
- * @author itutry
- * @create 2020-05-19_18:43
- */
+
 public class MeetingSystemV3 {
 
-  public static final ZoneId BEIJING_ZONEID = ZoneId.of("Asia/Beijing");
+  public static final ZoneId BEIJING_ZONEID = ZoneId.of("Asia/Shanghai");
   public static final ZoneId LONDON_ZONEID = ZoneId.of("Europe/London");
   public static final ZoneId CHICAGO_ZONEID = ZoneId.of("America/Chicago");
 
@@ -40,11 +29,11 @@ public class MeetingSystemV3 {
       ZonedDateTime chicagoMeetingZoneTime = beijinMeetingZoneTime.withZoneSameInstant(ZoneId.of("America/Chicago"));
       // 格式化新会议时间
       String showTimeStr = chicagoFormatter.format(chicagoMeetingZoneTime);
-      System.out.println("下一次会议时间" + showTimeStr);
+      System.out.println("next meeting time" + showTimeStr);
       Period between = Period.between(beijinNowTime.toLocalDate(), beijinMeetingZoneTime.toLocalDate());
       System.out.println("between,"+between);
     } else {
-      System.out.println("会议还没开始呢");
+      System.out.println("metting is not start");
     }
   }
 }
