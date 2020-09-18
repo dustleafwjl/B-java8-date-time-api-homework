5e6686bfb0288d28ca2c55f2f350b906cec9662c
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-18 12:14:01 +0800

    [jianglin] feat: finished practice 1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +16,17 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int year = date.getYear();
+    LocalDate lobar = getLobarDate(date, year);
+    return date.until(lobar, ChronoUnit.DAYS);
+  }
+
+  private static LocalDate getLobarDate(LocalDate date, int year) {
+    LocalDate lobar = LocalDate.of(year, Month.MAY, 1);
+    long until = date.until(lobar, ChronoUnit.DAYS);
+    if(until < 0) {
+      lobar = LocalDate.of(year + 1, Month.MAY, 1);
+    }
+    return lobar;
   }
 }
